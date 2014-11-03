8578a0ee0411cde1a3c08639118ee4dde8f30f5b
後藤 誉昌 <yasu1003@gmail.com>
2014-11-03 16:58:29 +0900

    Category初期データ定義

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# coding: utf-8
+
+Category.create(:title => 'graphic', :title_ja => 'イラスト・グラフィック')
+Category.create(:title => 'interior', :title_ja => '建築・インテリア・空間')
+Category.create(:title => 'product', :title_ja => 'プロダクト・パッケージ')
+Category.create(:title => 'degital', :title_ja => '写真・映像・デジタル')
+Category.create(:title => 'fashion', :title_ja => 'ファッション')
+Category.create(:title => 'installation', :title_ja => 'インスタレーション')
+Category.create(:title => 'seminar', :title_ja => 'セミナー・講演会')
+Category.create(:title => 'workshop', :title_ja => 'ワークショップ')
