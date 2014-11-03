1df072574221350bd20b03ae844acac4d4f7404a
後藤 誉昌 <yasu1003@gmail.com>
2014-11-03 16:22:30 +0900

    Postsテーブル変更

--- db/migrate/20141103013839_create_posts.rb
+++ db/migrate/20141103013839_create_posts.rb
@@ -5,10 +5,10 @@ class CreatePosts < ActiveRecord::Migration
       t.string  :title
       t.text    :body
       t.integer :category_id, :null => false, :index => true
-      t.open_date  :date,  :null => false
-      t.close_date  :date,  :null => false
-      t.open_time  :time,  :null => false
-      t.close_time  :time,  :null => false
+      t.date  :open_date,  :null => false
+      t.date  :close_date,  :null => false
+      t.time  :open_time,  :null => false
+      t.time  :close_time,  :null => false
       t.integer :charge,  :null => false
       t.string  :promoter,  :null => false
       t.string  :url
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,76 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141103014046) do
+
+  create_table "categories", force: true do |t|
+    t.string   "title",      null: false
+    t.string   "title_ja",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "favorites", force: true do |t|
+    t.integer  "user_id",    null: false
+    t.integer  "post_id",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "managers", force: true do |t|
+    t.string   "username",   null: false
+    t.string   "mail",       null: false
+    t.string   "password",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pictures", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "places", force: true do |t|
+    t.string   "title",                              null: false
+    t.string   "title_ja",                           null: false
+    t.text     "body"
+    t.string   "url"
+    t.string   "facebook"
+    t.string   "twitter"
+    t.string   "mail"
+    t.integer  "phone"
+    t.decimal  "latitude",   precision: 9, scale: 6
+    t.decimal  "longitude",  precision: 9, scale: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: true do |t|
+    t.string   "id_hash",     null: false
+    t.string   "title"
+    t.text     "body"
+    t.integer  "category_id", null: false
+    t.date     "open_date",   null: false
+    t.date     "close_date",  null: false
+    t.time     "open_time",   null: false
+    t.time     "close_time",  null: false
+    t.integer  "charge",      null: false
+    t.string   "promoter",    null: false
+    t.string   "url"
+    t.string   "facebook"
+    t.string   "twitter"
+    t.string   "mail"
+    t.integer  "manager_id",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.integer  "udid",       null: false
+    t.string   "sex",        null: false
+    t.integer  "birthday",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
