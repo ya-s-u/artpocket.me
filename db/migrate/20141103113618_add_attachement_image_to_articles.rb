3408640f1fa318d116e6cd633b54c191a9b987d2
Goto Yasuaki <yasu1003@gmail.com>
2014-11-12 11:23:16 +0900

    AddAttachmentImageToArticles削除

--- db/migrate/20141103113618_add_attachement_image_to_articles.rb
+++
@@ -1,9 +0,0 @@
-class AddAttachmentImageToArticles < ActiveRecord::Migration
-  def self.up
-    add_attachment :pictures, :image
-  end
-
-  def self.down
-    remove_attachment :pictures, :image
-  end
-end
