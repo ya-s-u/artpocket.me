dfe7a316d706e433ac45853f81ef70ff1382b968
後藤 誉昌 <yasu1003@gmail.com>
2014-11-13 22:04:10 +0900

    jquery-turbolinks対応

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -21,7 +21,7 @@
 })(jQuery);
 
 $.route(
-  {path: /.*\/posts\/\d+/,func: function() {
+  {path: /.*\/posts\/[0-9]+/,func: function() {
     showFunc();
   }},
   {path: /.*\/posts\/new/,func: function() {
@@ -39,35 +39,14 @@ $.route(
  * Post#index
  **********************************************/
 function indexFunc() {
-  $(window).load(function() {
-    $('.index_list').masonry({
-      itemSelector: 'li',
-      columnWidth: 300,
-      isFitWidth: true,
-    });
-  })
+  
 }
 
 /**********************************************
  * Post#show
  **********************************************/
 function showFunc() {
-   $(window).scroll(function () {
-     var s = $(this).scrollTop();
-     var r = $('.show_right_wrap').offset().top;
-     var l = $('.show_left').offset().top + $('.show_left').height();
-     var f = l-r-$('.show_right').height();
 
-     if (0 <= s && s <= r) {
-       $('.show_right').css('position','static');
-     } else if (s > r && s < f) {
-       $('.show_right').css('position','fixed');
-       $('.show_right').css('top','0');
-     } else if (s > f) {
-       $('.show_right').css('position','absolute');
-       $('.show_right').css('top',f);
-     }
-   })
  }
 
 /**********************************************
