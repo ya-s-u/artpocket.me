56239e312c7bbf566a696989cd5f2e7c058b3afa
後藤 誉昌 <yasu1003@gmail.com>
2014-12-03 23:49:58 +0900

    i18n設定

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Artpocket
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
 
-    I18n.enforce_available_locales = true
-    #config.i18n.default_locale = :ja
+    I18n.enforce_available_locales = false
+    config.i18n.default_locale = :ja
   end
 end
