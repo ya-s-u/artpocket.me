2d960d4e518e07a467f3c93a99f115a255d62b2f
後藤 誉昌 <yasu1003@gmail.com>
2015-01-16 00:43:22 +0900

    404,500追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Tablet uses PC view
   include Jpmobile::ViewSelector
   before_filter :disable_mobile_view_if_tablet
 
@@ -10,5 +9,18 @@ class ApplicationController < ActionController::Base
     if request.mobile && request.mobile.tablet?
       disable_mobile_view!
     end
+  end
+
+  # Exception
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404
+  rescue_from Exception, with: :render_500
+
+  def render_404(exception = nil)
+    render template: "errors/404", status: 404, layout: 'application'
+  end
+
+  def render_500(exception = nil)
+    render template: "errors/500", status: 500, layout: 'application'
   end
 end
