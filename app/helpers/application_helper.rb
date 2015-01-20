aee84de9b31e0502b33f0ff3fbe98c746306fecb
後藤 誉昌 <yasu1003@gmail.com>
2015-01-20 17:41:54 +0900

    SP/拡大禁止
    300ms遅延なくなる

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,7 +16,7 @@ module ApplicationHelper
         description: :description,
         locale: 'ja_JP'
       },
-      viewport: 'width=device-width,initial-scale=1.0',
+      viewport: 'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no',
     }
   end
 end
