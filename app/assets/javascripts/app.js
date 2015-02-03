3ada2b0661dab568a0064dc26ea09bd47a0bb1a4
後藤 誉昌 <yasu1003@gmail.com>
2015-02-04 01:28:44 +0900

    Angular無効化、ロードテスト

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,13 +1,13 @@
-'use strict';
-
-var App = angular.module('App', ['mainCtrl']);
-
-App.config(function($interpolateProvider) {
-  $interpolateProvider.startSymbol('[[');
-  $interpolateProvider.endSymbol(']]');
-})
-
-App.config(function($httpProvider) {
-  $httpProvider.defaults.xsrfCookieName = 'csrftoken';
-  $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-})
+// 'use strict';
+//
+// var App = angular.module('App', ['mainCtrl']);
+// //
+// // App.config(function($interpolateProvider) {
+// //   $interpolateProvider.startSymbol('[[');
+// //   $interpolateProvider.endSymbol(']]');
+// // })
+// //
+// // App.config(function($httpProvider) {
+// //   $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+// //   $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+// // })
